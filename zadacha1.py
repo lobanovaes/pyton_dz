cd8fc31ac2cfa9dda6fa323acd34ad001dd29375
evgeniay <es.trepova@mail.ru>
2022-11-25 21:46:49 +0700

    zd1

---
+++ zadacha1.py
@@ -0,0 +1,17 @@
+number = int(input('введите номер дня недели: '))
+if number == 1:
+    print('1 - нет')
+elif number == 2:
+    print('2 - нет')
+elif number == 3:
+    print('3 - нет')
+elif number == 4:
+    print('4 - нет')
+elif number == 4:
+    print('5 - нет')
+elif number == 6:
+    print('6 - да')
+elif number == 7:
+    print('7 - да')
+else: 
+    print ('введен неправильный номер')
