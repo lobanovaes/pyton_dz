781e4dc4c18c1a8e1ba912c772128fcd1b26ad07
evgeniay <es.trepova@mail.ru>
2022-11-28 23:25:02 +0700

    lec2

---
+++ lec2/HelloPyton.py
@@ -0,0 +1,7 @@
+def f(x):
+    if x == 1:
+        return 'Целое'
+    if x == 2.3:
+        return 23
+    else:
+        return
---
+++ lec2/lec2.py
@@ -0,0 +1,14 @@
+#colors = ['red', 'green', 'blue3']
+#data = open('file.txt', 'a')
+#data.writelines(colors)
+#data.write('\nLINE 12\n')
+#data.write('LINE 13\n')
+#data.write(colors)
+#data.close()
+
+
+
+
+import HelloPyton as h
+
+print(h .f(2.3))
